646596bb7b649e1864378a3bac2d9c27e81ac6f9
kaur <kaur@ciklum.com>
2023-03-24 14:54:15 +0500

    First commit

---
+++ java18/src/main/java/com/java17/example/Switch.java
@@ -0,0 +1,17 @@
+package com.java17.example;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final String b = "B";
+		switch (args[0]) {
+		case "A" -> System.out.println("Parameter is A");
+		case b -> System.out.println("Parameter is B");
+		default -> System.out.println("Parameter is unknown");
+		}
+
+	}
+
+}
